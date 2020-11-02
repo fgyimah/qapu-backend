cadd1712c378df37aa1344adf0f8d2f7517852ed
Francis Gyimah <fgyimah883@gmail.com>
2020-11-02 21:11:17 +0000

    Fixed handlers bug

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,14 @@
 import { Express, Router } from 'express';
 import auth from './auth';
+import department from './department';
+import faculty from './faculty';
 
 export interface Route {
   path: string;
   router: Router;
 }
 
-const routes: Route[] = [auth];
+const routes: Route[] = [auth, faculty, department];
 
 export default function createApi(app: Express) {
   routes.forEach((route) => {
